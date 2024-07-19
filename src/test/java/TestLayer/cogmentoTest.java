7b8625a9f22531af820bcc24449567f3648f854e
Prajakta <Prajaktayadav01@gmail.com>
2024-07-19 15:30:35 +0530

    Framework is created by prajakta

---
+++ src/test/java/TestLayer/cogmentoTest.java
@@ -0,0 +1,12 @@
+package TestLayer;
+
+import org.testng.annotations.Test;
+
+public class cogmentoTest {
+	 @Test
+	 public void login() {
+		   
+		 System.out.println("login testcase created by prajakta");
+	 }
+
+}
